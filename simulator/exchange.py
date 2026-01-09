20fa1d9b84df9877160e88c7c6bb0cafd2982614
mmaxtaylor2 <mmaxtaylor@gmail.com>
2026-01-09 15:27:43 -0700

    Update exchange.py

--- simulator/exchange.py
+++ simulator/exchange.py
@@ -1,1 +1,8 @@
+class Exchange:
+    def __init__(self, tick_size, latency, initial_price):
+        ...
+    def submit_order(...)
+    def match_orders(...)
+    def cancel_order(...)
+    def get_book(...)
 
